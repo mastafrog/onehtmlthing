4169185d04932f061dbba1316e9348a30d07db58
mastafrog <sergej.levitski@gmail.com>
2017-01-22 23:49:48 +0100

    zero point

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,31 @@
-var gulp        = require('gulp');
-//var browserSync = require('browser-sync');
-//var reload      = browserSync.reload;
-var rename      = require('gulp-rename');
-var sass        = require('gulp-sass');
- 
-// browser-sync task for starting the server.
-gulp.task('browser-sync', function() {
-//    //watch files
-//    var files = [
-//    './style.css',
-//    './*.php',
-//    './page-templates/*.php',
-//    './modules/*.php',
-//    './src/js/*.js'
-//    ];
-// 
-//    //initialize browsersync
-//    browserSync.init(files, {
-//    //browsersync with a php server
-//    proxy: "localhost:8080/orthofit/",
-//    notify: false
-//    });
-});
- 
-// Sass task, will run when any SCSS files change & BrowserSync
-// will auto-update browsers
+var gulp          = require('gulp')
+    ,rename       = require('gulp-rename')
+    ,sass         = require('gulp-sass')
+    ,sourcemaps   = require('gulp-sourcemaps')
+    ,prefix   = require('gulp-autoprefixer');
+
+
+
 gulp.task('sass', function () {
     return gulp.src('scss/*.scss')
-        .pipe(sass({ errLogToConsole: true,outputStyle: 'compressed' }))
-        .pipe(rename('style.css'))
-        .pipe(gulp.dest('./'))
-//        .pipe(reload({stream:true}));
+    .pipe(sourcemaps.init())
+        .pipe(sass({
+            errLogToConsole: true,
+            sourcemap: true,
+            style: 'compact' /*,
+            outputStyle: 'compressed'*/
+        }))
+        .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
+        .pipe(rename({
+            basename:   'style',
+        /*    suffix:     '.min',*/
+            extname:    '.css'
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./css/'));
 });
- 
-// Default task to be run with `gulp`
+
+
 gulp.task('default', ['sass', /*'browser-sync'*/], function () {
-    gulp.watch("scss/*.scss", ['sass']);
-    gulp.watch("scss/bootstrap/*.scss", ['sass']);
-    gulp.watch("scss/bootstrap/mixins/*.scss", ['sass']);
-    gulp.watch("scss/modules/*.scss", ['sass']);
-    gulp.watch("scss/partials/*.scss", ['sass']);
-    gulp.watch("scss/vendor/*.scss", ['sass']);
-    gulp.watch("scss/setup/*.scss", ['sass']);
+    gulp.watch("scss/**/*.scss", ['sass']);
 });
-
-
-//// FTP Deploy
-//var gutil = require( 'gulp-util' );
-//var ftp = require( 'vinyl-ftp' );
-//
-//gulp.task( 'deploy', function () {
-// 
-//    var conn = ftp.create( {
-//        host:     '',
-//        user:     '',
-//        password: '',
-//        parallel: 10,
-//        // log:      gutil.log
-//    } );
-// 
-//    var globs = [
-//        'src/**',
-//        'js/**',
-//        'fonts/**',
-//        '*.php',
-//        '*.json',
-//        '*.css'
-//    ];
-// 
-//    // using base = '.' will transfer everything to /public_html correctly 
-//    // turn off buffering in gulp.src for best performance 
-// 
-//    return gulp.src( globs, { base: '.', buffer: false } )
-//        .pipe( conn.newer( '' ) ) // only upload newer files 
-//        .pipe( conn.dest( '' ) );
-// 
-//} );
